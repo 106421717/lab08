70ad94aa840dfa380c226c6c54c977e788f32242
106421717 <106421717@student.swin.edu.au>
2026-05-26 20:21:06 +0800

    Initial commit

---
+++ search_form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Car Search</title>
+</head>
+<body>
+
+<h2>Search Car by Model</h2>
+
+<form action="search_results.php" method="GET">
+    <label for="model">Enter model:</label>
+    <input type="text" name="model" id="model">
+    <button type="submit">Search</button>
+</form>
+</body>
+</html>
